b264a99a148fa4eeff3ae417a4c7aaa41fcd2d59
TuRan <turanproc@outlook.com>
2025-05-11 17:18:22 +0800

    新增安装脚本以配置项目依赖

---
+++ install.sh
@@ -0,0 +1,11 @@
+pip install fastapi-0.87.0-py3-none-any.whl
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+pip install transformers==4.46.0
+pip install lightrag-hku==1.2.6
+pip install openai==1.69.0
+pip install pydantic==1.10.21
+pip install trl==0.17.0
+pip install peft==0.15.2
+pip install datasets==3.5.1
+pip install accelerate==1.6.0
+pip install pandas dotenv tiktoken pipmaster future uvicorn aiohttp, wandb
